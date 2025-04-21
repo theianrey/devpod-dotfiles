9fc28c40afe7d9f5e482f2446110cb416bad0515
theianrey <dlancermarco@gmail.com>
2025-04-21 11:55:40 +0800

    Update snacks config, enabled better escape, install regex language by default in treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -8,6 +8,13 @@ return {
       "lua",
       "vim",
       -- add more arguments for adding more treesitter parsers
+      "regex",
+      "vimdoc",
+    },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+      enabled = true,
     },
   },
 }
